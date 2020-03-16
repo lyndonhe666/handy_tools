a78f82842677330c8eb0c0963520ebad4797eca2
lyndonhe666 <lyndon_he@163.com>
2020-03-16 15:45:29 -0400

    update report_null

--- dp.py
+++ dp.py
@@ -7,4 +7,7 @@ def report_null(df):
     """
     Use to report detailed null value in a dataframe in pandas style
     """
-    return pd.DataFrame({col:sum(df[col].isnull()) for col in df.columns},index=range(1)).T.rename(columns={0:'column_name'})
+    res = pd.DataFrame({col:sum(df[col].isnull()) for col in df.columns},index=range(1)).T.reset_index().rename(columns={'index':'column_name',
+                                                                                                                        0:'number of null'})
+    res['percentage'] = res.apply(lambda x:100*(x['number of null']/len(df[x['column_name']])),axis=1)
+    return res
